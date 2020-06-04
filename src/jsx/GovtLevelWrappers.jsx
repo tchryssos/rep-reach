e5b0e1d4c8cfe68ee36c22af55676c61c8bc7904
Troy Chryssos <troychryssos@gmail.com>
2020-06-04 09:33:12 -0400

    styling for level wrappers

--- src/jsx/GovtLevelWrappers.jsx
+++ src/jsx/GovtLevelWrappers.jsx
@@ -6,23 +6,30 @@ import CityIcon from '/src/static/city.svg'
 import StateIcons from '/src/static/states.svg'
 import CapitolIcon from '/src/static/capitol-building.svg'
 
+import './GovtLevelWrappers.css'
+
 const LevelSection = ({ level, reps, city, state }) => {
 	const sectionData = { title: '', icon: '' }
 
-		if (level === 'locality') {
-			sectionData.title = city
-			sectionData.icon = CityIcon
-		} else if (includes('administrativeArea', level)) {
-			sectionData.title = `${state} State`
-			sectionData.icon = StateIcons
-		} else {
-			sectionData.title = 'National'
-			sectionData.icon = CapitolIcon
-		}
+	if (level === 'locality') {
+		sectionData.title = city
+		sectionData.icon = CityIcon
+	} else if (includes('administrativeArea', level)) {
+		sectionData.title = `${state} State`
+		sectionData.icon = StateIcons
+	} else {
+		sectionData.title = 'National'
+		sectionData.icon = CapitolIcon
+	}
 
 	return (
 		<div class="levelSection">
-			<h2>{sectionData.title}</h2>
+			<div class="sectionHeader">
+				<div class="sectionTitleWrapper">
+					<h2>{sectionData.title}</h2>
+				</div>
+				<div class="sectionDivider" />
+			</div>
 		</div>
 	)
 }
