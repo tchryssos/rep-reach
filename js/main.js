8c8ea2ede5d4996443d643c87481bc810459feed
Troy Chryssos <troychryssos@gmail.com>
2020-06-03 20:21:51 -0400

    splitting civic fetch and submit funcs

--- js/main.js
+++ js/main.js
@@ -2,12 +2,19 @@ import 'regenerator-runtime/runtime'
 
 // FORM SUBMIT
 const zipForm = document.querySelector('#zipForm')
+
+const fetchCivicData = async (zip) => {
+	const resp = await fetch(
+		`https://www.googleapis.com/civicinfo/v2/representatives?address=${zip}&key=${process.env.CIVIC_API_KEY}`,
+	)
+	return resp.json()
+}
 const submitZipcode = async (e) => {
 	e.preventDefault()
 	const zipcode = document.querySelector('#zip').value
-	const resp = await fetch(
-		`https://www.googleapis.com/civicinfo/v2/representatives?address=${zipcode}&key=${process.env.CIVIC_API_KEY}`,
-	)
-	console.log(resp)
+	fetchCivicData(zipcode)
+		.then((data) => {
+			console.log(data)
+		})
 }
 zipForm.addEventListener('submit', submitZipcode)
