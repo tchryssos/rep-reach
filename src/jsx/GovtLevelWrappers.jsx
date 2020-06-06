cd3f84cb557cbd88a354fae287d095aaf8ff3166
Troy Chryssos <troychryssos@gmail.com>
2020-06-06 15:31:14 -0400

    breaking reps out into new component

--- src/jsx/GovtLevelWrappers.jsx
+++ src/jsx/GovtLevelWrappers.jsx
@@ -1,38 +1,40 @@
 import { h, Fragment } from 'preact'
 import map from 'ramda/src/map'
-import includes from 'ramda/src/includes'
-
-import CityIcon from '/src/static/city.svg'
-import CapitolIcon from '/src/static/capitol-building.svg'
+import prop from 'ramda/src/prop'
 
 import StateDefsSvg from '/src/jsx/StateDefsSvg.jsx'
+import Rep from '/src/jsx/Rep.jsx'
 
 import './GovtLevelWrappers.css'
 
-const LevelSection = ({ level, reps, city, state }) => {
-	const sectionData = { title: level, icon: '' }
+const Reps = ({ reps }) => map(
+	({
+		officeName, officialName, officialChannels, officialEmails, officialPages,
+		officialPhones,
+	}) => (
+		<Rep
+			officeName={officeName}
+			officialChannels={officialChannels}
+			officialName={officialName}
+			officialEmails={officialEmails}
+			officialPages={officialPages}
+			officialPhones={officialPhones}
+		/>
+	),
+	reps,
+)
 
-	// if (level === '') {
-	// 	sectionData.icon = CityIcon
-	// } else if (includes('administrativeArea', level)) {
-	// 	sectionData.title = `${state} State`
-	// 	// sectionData.icon = StateIcons
-	// } else {
-	// 	sectionData.title = 'National'
-	// 	sectionData.icon = CapitolIcon
-	// }
-
-	return (
-		<div class="levelSection">
-			<div class="sectionHeader">
-				<div class="sectionTitleWrapper">
-					<h2>{sectionData.title}</h2>
-				</div>
-				<div class="sectionDivider" />
+const LevelSection = ({ level, reps, city, state }) => (
+	<div class="levelSection">
+		<div class="sectionHeader">
+			<div class="sectionTitleWrapper">
+				<h2>{level}</h2>
 			</div>
+			<div class="sectionDivider" />
 		</div>
-	)
-}
+		<Reps reps={prop(level, reps)} />
+	</div>
+)
 
 const GovtLevelWrappers = ({ levels, reps, city, state }) => {
 	if (!levels.length) {
---
+++ src/jsx/Rep.jsx
@@ -0,0 +1,10 @@
+import { h } from 'preact'
+
+const Rep = ({
+	officeName, officialName, officialChannels, officialEmails, officialPages,
+	officialPhones,
+}) => {
+	return <div>rep</div>
+}
+
+export default Rep
