9d162894ba39d96e2d8719dfc63d239438fa2e6b
Troy Chryssos <troychryssos@gmail.com>
2020-06-06 11:23:25 -0400

    adding street address for more rep results

--- src/jsx/AddressForm.jsx
+++ src/jsx/AddressForm.jsx
@@ -1,31 +1,47 @@
 import { h } from 'preact'
 import { useState } from 'preact/hooks'
+import assoc from 'ramda/src/assoc'
+import prop from 'ramda/src/prop'
 
-import submitZipCode from '/src/logic/submitZipCode'
+import submitAddress from '/src/logic/submitAddress'
 
 import './AddressForm.css'
 
 const AddressForm = ({ setReps, setState, setCity }) =>  {
-	const [value, setValue] = useState('')
+	const [value, setValue] = useState({
+		street: '',
+		zip: '',
+	})
+	console.log(value)
 
 	const onSubmit = async (e) => {
 		e.preventDefault()
-		await submitZipCode(value, setValue, setReps, setState, setCity)
+		await submitAddress(value, setValue, setReps, setState, setCity)
 	}
 
 	const onInput = (e) => {
-		const { value } = e.target
-		setValue(value)
+		const { value: inputValue, id } = e.target
+		setValue(assoc(id, inputValue, value))
 	}
 
 	return (
 		<form id="addressForm" onSubmit={onSubmit}>
+			<label for="street">
+				Street Address:
+				<input
+					id="street"
+					name="street"
+					value={prop('street', value)}
+					type="text"
+					onInput={onInput}
+				/>
+			</label>
 			<label for="zip">
 				Zipcode:
 				<input
 					id="zip"
 					name="zip"
-					value={value}
+					value={prop('zip', value)}
 					type="text"
 					onInput={onInput}
 				/>
